33c53e97d912568b0a93f24b92e3cec4762048bf
charith <tilershub@gmail.com>
2026-01-05 16:52:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -49,6 +49,27 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
     ];
   },
 };
