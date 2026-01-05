fd33529ed0bcff8a4ce0a26689119aa8564e7aff
charith <tilershub@gmail.com>
2026-01-05 10:29:56 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,17 +5,18 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
 
   const code = searchParams.get("code");
-  const nextParam = searchParams.get("next");
-  const next = nextParam && nextParam.startsWith("/") ? nextParam : "/";
+  const next = searchParams.get("next") ?? "/";
 
-  if (code) {
-    const supabase = createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-
-    if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
-    }
+  if (!code) {
+    return NextResponse.redirect(`${origin}/auth`);
   }
 
-  return NextResponse.redirect(`${origin}/auth`);
+  const supabase = createClient();
+  const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+  if (error) {
+    return NextResponse.redirect(`${origin}/auth`);
+  }
+
+  return NextResponse.redirect(`${origin}${next}`);
 }
